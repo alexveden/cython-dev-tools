ac0fe3ef3715b420fe3a6d077c56cc3358cced9e
Alex Veden <i@alexveden.com>
2022-08-11 23:55:54 +0400

    - spelling

--- src/cython_tools/_cy_tools_samples/with_c_code/call_mymath.pyx
+++ src/cython_tools/_cy_tools_samples/with_c_code/call_mymath.pyx
@@ -1,8 +1,8 @@
 # distutils: sources = cy_tools_samples/with_c_code/mymath.c
 #     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # IMPORTANT: you must include this line to get mymath.c module compiled in the call_mymath.pyx (path must be relative to project root!)
-#            comma separated source is allowed.
-#                 otherwise you will have to setup dynamic linking https://github.com/cython/cython/tree/master/Demos/libraries
+#            comma separated source files are allowed too.
+# Otherwise, you will have to setup dynamic linking https://github.com/cython/cython/tree/master/Demos/libraries
 
 """
 Simple example how to include external .c code
