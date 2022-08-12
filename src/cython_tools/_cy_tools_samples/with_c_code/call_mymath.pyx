5ca6cd76e3394aa90151fd399788f43e7d80914e
Alex Veden <i@alexveden.com>
2022-08-12 12:46:27 +0400

    - call_mymath.pyx - refactored with include dir

--- src/cython_tools/_cy_tools_samples/with_c_code/call_mymath.pyx
+++ src/cython_tools/_cy_tools_samples/with_c_code/call_mymath.pyx
@@ -1,7 +1,8 @@
-# distutils: sources = cy_tools_samples/with_c_code/mymath.c
+# distutils: sources = cy_tools_samples/with_c_code/include/mymath.c
+# distutils: include_dirs = cy_tools_samples/with_c_code/include
 #     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # IMPORTANT: you must include this line to get mymath.c module compiled in the call_mymath.pyx (path must be relative to project root!)
-#            comma separated source files are allowed too.
+#            SPACE separated source files are allowed too. (documentation say # distutils: sources = helper.c, another_helper.c, but this doesn't work!)
 # Otherwise, you will have to setup dynamic linking https://github.com/cython/cython/tree/master/Demos/libraries
 
 """
@@ -11,7 +12,7 @@ Cython Tools run command:
 cytool run cy_tools_samples/with_c_code/call_mymath.pyx@call_sinc
 """
 
-cdef extern from "mymath.h":
+cdef extern from "include/mymath.h":
     double sinc(double)
 
 
